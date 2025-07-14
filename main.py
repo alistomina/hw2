dfa104d2975c3b5b5d6aefa832fcc82e1044d842
alistomina <alinaistomina@yandex.ru>
2025-07-14 21:51:52 +0300

    execution homework task create commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-name = Alina
+name = "Alina"
 age = 28
-work = QA Automation Engineer
+work = "QA Automation Engineer"
 print(f"Hi my name is {name}")
 print(f"I'm {age} years old")
 print(f"I'm working {work}")
